e9436c2e7453d38a90ef0fe3187dfc511f536bfa
Aditya singh <6306043783s@gmail.com>
2024-12-31 14:24:55 +0530

    improve promotionsBannerDetail

--- src/Components/PromotionsBannerDetail.jsx
+++ src/Components/PromotionsBannerDetail.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { ConfigProvider, Modal } from 'antd'
+import { useLocalStorage } from '../Hooks/useLocalStorage'
 const StarBoxContent = React.lazy(() => import('./StarBoxContent'));
 const LoginModal = React.lazy(() => import('./LoginPage/LoginModal'));
 
 const PromotionsBannerDetail = () => {
+    const [isUserLogin, setIsUserLogin] = useLocalStorage('User', {});
     const [Open, setOpen] = useState(false);
     return (
         <main>
@@ -20,10 +22,32 @@ const PromotionsBannerDetail = () => {
                         <p>After successful payment, the specified number of beverages will be added to your account. You can view these beverages under the "Pay/ Rewards" section of the mobile app.</p>
                         <p className='text-[#00b050]'>Book your subscription Today!</p>
                     </div>
-                    <div className='flex justify-center mt-10'>
-                        <button onClick={() => setOpen(true)} className='bg-[#00754a] hover:bg-[#1e3932] px-28 py-2 rounded-3xl font-bold text-[12px] text-white'>Login To view offers</button>
-                    </div>
                 </div>
+                {Object.keys(isUserLogin).length ?
+                    <div className='relative -left-5 bg-[#edebe9] px-4 lg:px-8 py-8 w-screen min-h-[400px]'>
+                        <div className='m-auto py-5 max-w-[1240px]'>
+                            <img className='top-0 right-5 absolute w-14' src="https://www.starbucks.in/assets/icon/greyleafright.svg" alt="greyLeafright" />
+                            <h1 className='font-bold text-xl'>Beverage Subscription</h1>
+                            <div className='flex flex-col items-start bg-white mt-8 rounded-xl w-full max-w-[340px]'>
+                                <div className='flex-grow rounded-t-2xl w-full h-[150px] overflow-hidden'>
+                                    <img className='h-full object-cover' src="https://starbucksstatic.cognizantorderserv.com/Items/Large/webP/109732.webp" loading='lazy' alt="" />
+                                </div>
+                                <div className='flex flex-col items-start gap-14 p-4 w-full'>
+                                    <div>
+                                        <img src="https://www.starbucks.in/assets/icon/veg.svg" loading='lazy' alt="" />
+                                        <h1 className='text-sm'>Buy 3 Tall core Beverages for ₹789</h1>
+                                        <p className='text-[10px]'>Get 3 Tall core beverages at ₹789 with 30 days validity.</p>
+                                    </div>
+                                    <div className='flex justify-between w-full'>
+                                        <span className='font-bold'>₹ 789.00</span>
+                                        <button className='bg-[#00754a] hover:bg-[#1e3932] shadow-5xl px-4 py-2 rounded-3xl font-bold text-white text-xs'>Add Item</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> : <div className='flex justify-center mt-10'>
+                        <button onClick={() => setOpen(true)} className='bg-[#00754a] hover:bg-[#1e3932] px-28 py-2 rounded-3xl font-bold text-[12px] text-white'>Login To view offers</button>
+                    </div>}
                 <div className="border-gray-300 border-b-2">
                     <h1 className="m-auto mt-2 py-4 max-w-[720px] lg:max-w-[1240px] font-[600] font-[sans-serif] text-2xl">Terms & Conditions</h1>
                 </div>
